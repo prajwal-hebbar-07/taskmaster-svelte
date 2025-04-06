f2f09ce4b358344eafdc7b600be401dfedac438c
Hebbar <prajwalhebbaras@gmail.com>
2025-04-06 16:29:46 +0530

    update typography styles and add Google Fonts; include new cover images in HeroSection

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,5 +1,22 @@
+<script>
+  import bookCover from "$assets/book_cover.png";
+  import phoneCover from "$assets/phone_cover.png";
+</script>
+
 <section class="hero">
   <div class="hero-showcase text-center bold">
-    <h1 class="purple">MOVE TO</h1>
+    <h1 class="purple">Move To</h1>
+    <h1 class="purple mb-xs">Task Master</h1>
+    <h4 class="italic">There is no need to struggle like I did.</h4>
+    <img
+      class="book-cover"
+      src={bookCover}
+      alt="Computer Screen for TaskMaster"
+    />
+    <img
+      src={phoneCover}
+      alt="Phone Screen for TaskMaster"
+      class="phone-cover"
+    />
   </div>
 </section>
