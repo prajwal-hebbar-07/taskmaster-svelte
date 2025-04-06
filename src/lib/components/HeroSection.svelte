dfd8e3cb09b00a5daf3ca4914aa870c6b33969e4
Hebbar <prajwalhebbaras@gmail.com>
2025-04-06 17:25:20 +0530

    add hero text section with descriptive content and styles

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -19,4 +19,59 @@
       class="phone-cover"
     />
   </div>
+  <div class="hero-text bold white text-center">
+    <h1>This Application</h1>
+    <h1>Will Help You</h1>
+    <h1>Save Your Time</h1>
+    <h1 class="mb-m">And Efforts</h1>
+    <p class="light-grey semi-bold italic">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab iure dolorem
+      accusantium tempora, consectetur modi non maiores! Corporis numquam ut
+      iste fugit, quae temporibus reprehenderit recusandae dignissimos, iure
+      totam expedita.
+    </p>
+  </div>
 </section>
+
+<style>
+  .hero {
+    display: flex;
+    height: 100vh;
+    overflow-y: hidden;
+  }
+
+  .hero-showcase {
+    position: relative;
+    width: 50%;
+    padding: 100px 16px 0 16px;
+    background: linear-gradient(
+      135deg,
+      #e2cbff,
+      #e2cbff 25%,
+      #deeaff 50%,
+      #ece5ff 75%,
+      #927bce
+    );
+  }
+
+  .hero-text {
+    padding: 100px 5%;
+    background-color: black;
+    width: 50%;
+  }
+
+  .book-cover {
+    position: absolute;
+    right: 2%;
+    bottom: 5%;
+    width: 65%;
+    /* transform: translateY(10%); */
+  }
+
+  .phone-cover {
+    position: absolute;
+    right: 60%;
+    bottom: 20%;
+    width: 30%;
+  }
+</style>
